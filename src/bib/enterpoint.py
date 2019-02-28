f007317d810b09c05d95d1d8f69585ce36fef9c9
xxc <x007007007@hotmail.com>
2019-02-28 22:52:47 +0800

    refactor: enterpoint

--- src/bib/enterpoint.py
+++ src/bib/enterpoint.py
@@ -1,81 +1,115 @@
 import os
 import argparse
-import glob
-
+from six import with_metaclass
 
 from . import core
 
 
-def parse_arg():
-    parser = argparse.ArgumentParser(prog='bib')
+class Command(object):
+    _sub_command = []
 
-    subparsers = parser.add_subparsers()
+    def __init__(self):
+        self._parser = argparse.ArgumentParser(prog='bib')
+        self._subparsers = self._parser.add_subparsers()
+        self.init_sub_command()
 
-    parsers_init = subparsers.add_parser('init', help='show repo state')
-    parsers_init.add_argument('path', nargs="*", default=".")
-    parsers_init.set_defaults(func=init)
+    def init_sub_command(self):
+        for sub_cmd_cls in self._sub_command:
+            sub_cmd = sub_cmd_cls()  # type: SubCommandBase
+            parser = self._subparsers.add_parser(sub_cmd.name, help=sub_cmd.help_text)
+            parser.set_defaults(func=sub_cmd.handle)
+            sub_cmd.set_parser(parser)
 
-    parsers_state = subparsers.add_parser('state', help='show repo state')
-    parsers_state.set_defaults(func=state)
-
-    parser_cmd_add = subparsers.add_parser('add', help='add file into index')
-    parser_cmd_add.add_argument('match_pattern', nargs="+")
-    parser_cmd_add.set_defaults(func=cmd_add)
-
-    parser_cmd_remove = subparsers.add_parser(
-        'rm', help='remove file index from index'
-    )
-    parser_cmd_remove.add_argument('match_pattern', nargs="+")
-    parser_cmd_remove.set_defaults(func=cmd_rm)
-
-    parser_cmd_status = subparsers.add_parser(
-        'status', help="show datebase status"
-    )
-    parser_cmd_status.set_defaults(func=cmd_status)
-
-    parser_cmd_check = subparsers.add_parser(
-        'check', help='check file hash'
-    )
-    parser_cmd_check.add_argument('match_pattern', nargs="+")
-    parser_cmd_check.set_defaults(func=cmd_check)
-
-    return parser
-
-def init(args):
-    repo_path = os.path.abspath(os.path.join(os.curdir, args.path))
-    bib_repo = core.BibRepo.create_bib_repo(repo_path)
-
-def state(args):
-    bib_repo = core.BibRepo.get_bib_repo(os.curdir)
-    print(bib_repo)
-
-def cmd_add(args):
-    bib_repo = core.BibRepo.get_bib_repo(os.curdir)
-    bib_repo.add_resource(*args.match_pattern)
-
-def cmd_rm(args):
-    bib_repo = core.BibRepo.get_bib_repo(os.curdir)
-    bib_repo.remove_resource(*args.match_pattern)
+    def execute(self):
+        args = self._parser.parse_args()
+        if hasattr(args, 'func'):
+            args.func(args)
+        else:
+            self._parser.print_help()
 
 
-def cmd_status(args):
-    bib_repo = core.BibRepo.get_bib_repo(os.curdir)
-    res = bib_repo.status()
-    print(f"we have {res['index_count']} resources, "
-          f"{res['unique_count']} are unique, "
-          f"{res['unindex_count']} untracked")
+class CommandMeta(type):
+    def __new__(self, cls, bases, attr):
+        is_cmd = cls.endswith('Command')
+        if 'name' not in attr:
+            attr['name'] = cls[:-7].lower()
+        sub_command = type.__new__(self, cls, bases, attr)
+        if is_cmd:
+            Command._sub_command.append(sub_command)
+        return sub_command
 
-def cmd_check(args):
-    bib_repo = core.BibRepo.get_bib_repo(os.curdir)
-    res = bib_repo.check_files(*args.match_pattern)
-    print(res)
+
+class SubCommandBase(with_metaclass(CommandMeta)):
+    help_text = ''
+
+    def set_parser(self, parser):
+        pass
+
+    def handle(self, args):
+        pass
+
+
+
+class RepoCommandMixin(object):
+    @property
+    def repo(self):
+        self._repo = core.BibRepo.get_bib_repo(os.curdir)
+        return self._repo
+
+
+class InitCommand(SubCommandBase):
+    help_text = "init bib repo"
+    def set_parser(self, parser):
+        parser.add_argument('path', nargs="*", default=".")
+
+    def handle(self, args):
+        repo_path = os.path.abspath(os.path.join(os.curdir, args.path))
+        bib_repo = core.BibRepo.create_bib_repo(repo_path)
+
+
+class StatusCommand(SubCommandBase, RepoCommandMixin):
+    help_text = 'show repo state'
+
+    def handle(self, args):
+        res = self.repo.status()
+        print(f"we have {res['index_count']} resources, "
+              f"{res['unique_count']} are unique, "
+              f"{res['unindex_count']} untracked")
+
+
+class AddCommand(SubCommandBase, RepoCommandMixin):
+    help_text = 'add file into index'
+
+    def set_parser(self, parser):
+        parser.add_argument('match_pattern', nargs="+")
+
+    def handle(self, args):
+        self.repo.add_resource(*args.match_pattern)
+
+
+class RemoveCommand(SubCommandBase, RepoCommandMixin):
+    def set_parser(self, parser):
+        parser.add_argument('match_pattern', nargs="+")
+
+    def handle(self, args):
+        self.repo.remove_resource(*args.match_pattern)
+
+
+class CheckCommand(SubCommandBase, RepoCommandMixin):
+    def set_parser(self, parser):
+        parser.add_argument('match_pattern', nargs="+")
+
+    def handle(self, args):
+        res = self.repo.check_files(*args.match_pattern)
+        print(res)
+
+class ScanCommand(SubCommandBase, RepoCommandMixin):
+    help_text = 'scan folder, automatic add, remote by rule'
+
+    def handle(self, args):
+        pass
 
 
 def bib():
-    parser = parse_arg()
-    args = parser.parse_args()
-    if hasattr(args, 'func'):
-        args.func(args)
-    else:
-        parser.print_help()
-
+    command = Command()
+    command.execute()
